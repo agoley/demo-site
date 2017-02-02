67c3fc4b5aac9536e5b7bc9c4e34edbba793a74a
Kris Redding <kredding@autoscribe.com>
2017-02-02 15:53:07 -0500

    fix resize bug

--- app/directives/heroimg.directive.js
+++ app/directives/heroimg.directive.js
@@ -5,7 +5,19 @@ directives.directive('heroImg', ['$window', function($window) {
       //640px
       if($window.outerWidth < 640){
         element.css({'height': ($window.outerHeight - 135) });
-      }      
+      }
+
+      //$window.resize(function resize(){
+      angular.element($window).bind('resize', function(){
+        if($window.outerWidth < 640){
+          element.css({'height': ($window.outerHeight - 135) });
+        }
+        else {
+          element.removeAttr("style");
+          //element.style.height = null;
+        }
+      });
+
     }
   }
 }]);
