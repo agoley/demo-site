4b0c77ccfbd56c2902ddcf840839250f387f5210
Kris Redding <kredding@autoscribe.com>
2017-02-02 16:05:22 -0500

    update fix

--- app/directives/heroimg.directive.js
+++ app/directives/heroimg.directive.js
@@ -2,19 +2,24 @@ directives.directive('heroImg', ['$window', function($window) {
   return {
     restrict: 'EA',
     link: function ($scope, element, attrs) {
-      //640px
-      if($window.outerWidth < 640){
+      var mobileSize = 640;
+
+      var initialWidth = $window.outerWidth;
+      if($window.outerWidth < mobileSize){
         element.css({'height': ($window.outerHeight - 135) });
       }
 
       //$window.resize(function resize(){
       angular.element($window).bind('resize', function(){
-        if($window.outerWidth < 640){
-          element.css({'height': ($window.outerHeight - 135) });
-        }
-        else {
-          element.removeAttr("style");
-          //element.style.height = null;
+        if(initialWidth != $window.outerWidth){
+          if($window.outerWidth < mobileSize){
+            element.css({'height': ($window.outerHeight - 135) });
+          }
+          else {
+            element.removeAttr("style");
+            //element.style.height = null;
+          }
+          initialWidth = $window.outerWidth;
         }
       });
 
